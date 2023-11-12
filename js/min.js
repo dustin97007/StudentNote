4abd6d10588bbc8d4ddf0fdbfba83ce2a808500a
Lokmani-ibrahim <148336289+Islem-Touati@users.noreply.github.com>
2023-11-12 16:06:57 +0100

    add JS

--- js/min.js
+++ js/min.js
@@ -1,28 +1,17 @@
-const fullName = document.querySelector('.fullName');
+//recuperation des valeurs des champs du formulaire
+// const fullName = document.getElementById('text1');
+//console.log(fullName.textContent); affichage fullName
+const fullName = document.querySelector('.fullName');//ye5ou donnee eli fi wosot fullname
 const dateOfBirth = document.querySelector('.dateOfBirth');
 const gender = document.querySelector('select[name="gender"]');
 const task1 = document.querySelector('.task1');
 const task2 = document.querySelector('.task2');
 const task3 = document.querySelector('.task3');
-
-
-
-
+//recupere les buttons
 const result = document.querySelector('.result');
 const submit = document.querySelector('.submit');
 const reset = document.querySelector('.reset');
-
-function validateFullName(fullName {
-        if (!fullName) {
-            return " Full name is required.";
-
-        }
-        if (fullName.length < 3 || fullName.length > 20) {
-            return " Full name must be between 3 and 20 characters long. ";
-        }
-        if (!/^[a-zA-Z ]+$/.test(fullName)) {
-            return " Full name must contain only letters and spaces."
-        }
-        return null;
-
-    }
+// validation rules fullName
+function validateFullName(fullName) {
+    
+}
